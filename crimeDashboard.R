5889ff196d220c06487e7e269da6656563b847bb
unknown <vernonvernon29@gmail.com>
2019-11-06 10:06:15 +0200

    1.1.3

--- crimeDashboard.R
+++ crimeDashboard.R
@@ -1,9 +1,32 @@
 library(shiny)
 library(shinydashboard)
+library(DT)
+library(dplyr)
 
 
 interface <- dashboardPage(
   dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardSidebar(
+    uiOutput('select_category')
+  )
 )
+
+logic <- function(input, output){
+  
+  #load data from file
+  load_data <- reactive({
+    data = read.csv('crime_stats.csv')
+  })
+  
+  #create select input: user can select crime category
+  output$select_category <- renderUI({
+    selectInput(
+      inputId = 'crime_category',
+      label = 'Select crime category',
+      choices = load_data() %>% select(Category)
+    )
+  })
+  
+}
+
+shinyApp(interface, logic)
